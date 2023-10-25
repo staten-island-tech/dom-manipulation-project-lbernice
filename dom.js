fbb8e90a6ec1daef4f8436bb1e88adf45bc63070
lbernice <bernicel677@gmail.com>
2023-10-25 12:40:15 -0400

    f

--- dom.js
+++ dom.js
@@ -1,3 +1,9 @@
+// -Create an object called “DOMSelectors” to hold your DOM Selectors
+// -Create a function that creates an object and calls the following functions
+// -Create a function that injects the newly created object into the DOM
+// -Create a function that clears the input fields after injecting the object
+// -Create a function to remove an object after they have been created
+
 const form = document.querySelector("#form");
 const firstName = document.querySelector(".first-name")
 console.log(firstName);
