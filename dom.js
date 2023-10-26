53302d3fa576381c2e12f3afa46af9e1d531996f
lbernice <bernicel677@gmail.com>
2023-10-26 00:34:35 -0400

    g

--- dom.js
+++ dom.js
@@ -1,16 +1,24 @@
-const form = document.querySelector("#form");
-const firstName = document.querySelector(".first-name")
-console.log(firstName);
+// const form = document.querySelector("#form");
+// const firstName = document.querySelector(".first-name")
+// console.log(firstName);
+
+// DOMSelectors = {
+//     // store all related code for selecting elements here
+//     form: document.querySelector("#form"),
+//     firstName: document.querySelector(".first-name"),
+//     h2s: document.querySelectorAll("h2"),
+// };
+// DOMSelectors.form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     DOMSelectors.h2s.forEach(
+//         (header) => (header.textContent = DOMSelectors.firstName.value)
+//     );
+// });
 
 DOMSelectors = {
-    // store all related code for selecting elements here
-    form: document.querySelector("#form"),
-    firstName: document.querySelector(".first-name"),
-    h2s: document.querySelectorAll("h2"),
-};
-DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    DOMSelectors.h2s.forEach(
-        (header) => (header.textContent = DOMSelectors.firstName.value)
-    );
-});
+    form:document.querySelector("#form"),
+    starType: document.querySelector(".star-type"),
+    starName: document.querySelector(".star-name"),
+    starMonth: document.querySelector(".star-month"),
+    image: document.querySelector(".image")
+}
